7bc3f690000ffec9f67a07fcabb9adb905a65e69
Rdyx <tiranico.pro@gmail.com>
2021-01-18 11:19:16 +0100

    Rework oops handling, update reqs, add items pages

--- project/items/urls.py
+++ project/items/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    # path("<int:race_id>/", views.user_signup_view, name="material"),
-    # path("<int:material_id>/", views.user_signup_view, name="race"),
-    # path("<int:item_id>/", views.user_signup_view, name="item"),
+    path("r/<int:race_id>/", views.show_race_view, name="material"),
+    path("m/<int:material_id>/", views.show_material_view, name="race"),
+    path("i/<int:item_id>/", views.show_item_view, name="item"),
 ]
--- project/items/views.py
+++ project/items/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+from .models import RaceModel, MaterialModel, ItemModel
 
 
 def show_race_view(request, race_id):
+    try:
+        race = RaceModel.objects.get(race_id=race_id)
+    except RaceModel.DoesNotExist:
+        return redirect("oops")
+
+    context = {"race": race}
     return render(request, "race.html", context)
 
 
 def show_material_view(request, material_id):
+    try:
+        material = MaterialModel.objects.get(material_id=material_id)
+    except MaterialModel.DoesNotExist:
+        return redirect("oops")
+
+    context = {"material": material}
     return render(request, "material.html", context)
 
 
 def show_item_view(request, item_id):
+    try:
+        item = ItemModel.objects.select_related("item_race", "item_material").get(
+            item_id=item_id
+        )
+    except ItemModel.DoesNotExist:
+        return redirect("oops")
+
+    context = {"item": item}
     return render(request, "item.html", context)
